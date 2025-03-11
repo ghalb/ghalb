5a0288464b312e4759863980ac7588b6fe4a9229
ghalb <ghalb@users.noreply.github.com>
2025-03-10 22:42:20 -0700

    Add files via upload

---
+++ script01.js
@@ -0,0 +1,24 @@
+addEventListener('fetch', event => {
+  event.respondWith(handleRequest(event.request));
+});
+
+async function handleRequest(request) {
+  const url = new URL(request.url);
+
+  // تغییر آدرس مقصد به دامنه‌ی مورد نظر شما
+  const targetUrl = 'https://ghalb-github-io.pages.dev' + url.pathname + url.search;
+
+  // ارسال درخواست به سرور مقصد
+  const response = await fetch(targetUrl, {
+    method: request.method,
+    headers: request.headers,
+    body: request.body
+  });
+
+  // بازگرداندن پاسخ به کاربر
+  return new Response(response.body, {
+    status: response.status,
+    headers: response.headers
+  });
+}});
+}
